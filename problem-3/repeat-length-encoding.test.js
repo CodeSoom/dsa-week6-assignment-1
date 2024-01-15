9afede9e6ca566e4ac632eb761ae28f9af7cf056
JiHo Kim <kr36510@naver.com>
2024-01-15 22:35:31 +0900

    problem-3

--- problem-3/repeat-length-encoding.test.js
+++ problem-3/repeat-length-encoding.test.js
@@ -1,11 +1,26 @@
 const encode = (words) => {
+  const answer = [];
+
+  for (const input of words) {
+    answer.push(input[0] + input.length);
+  }
+
+  return answer.join('');
 };
 
 const decode = (string) => {
+  const answer = [];
+
+  for (let i = 0; i < string.length - 1; i += 2) {
+    const count = Number(string[i + 1]);
+    answer.push(string[i].repeat(count));
+  }
+
+  return answer;
 };
 
 test('repeat length encoding', () => {
-  const origin = ['a', 'aa', 'aaa', 'aaaa', 'aaaaa', 'aaaaaa', 'aaaaaaaa', 'aaaaaaaa'];
+  const origin = ['a', 'aa', 'aaa', 'aaaa', 'aaaaa', 'aaaaaa', 'aaaaaaa', 'aaaaaaaa'];
 
   const encodedString = encode(origin);
 
